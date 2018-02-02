fd1acbea69c703f2673b8b898fc93cd03987d7f1
mahavivo <mahavivo@126.com>
2018-02-02 08:29:39 +0800

    init

--- twitter_robot.py
+++ twitter_robot.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
 import json
 import re
+import os
+
 import twitter
 
-# replace 'default' with twitter user
-user = 'mahavivo'
-# path to json file with your twitter credentials (see https://python-twitter.readthedocs.io/en/latest/)
+
+# 替换此处的'default'为自己想使用的用户名
+user = 'default'
+
+# 在json文件中填入从twitter获得的授权账号和钥匙（credentials）。 (see https://python-twitter.readthedocs.io/en/latest/)
 creds = 'twitter_creds.json'
-file_name = 'mahavivo.txt'
+
+file_name = '%s_tweets.txt' % user
 
 
 def init_twitter():
-    # load twitter API tokens；在twitter_creds.json文件中分别填入从网页获得的授权凭证。
+    # load twitter API tokens
     with open(creds) as data_file:
         data = json.load(data_file)
 
@@ -36,11 +40,11 @@ def main():
     # # 更新Twitter
     # status = api.PostUpdate('更新测试!')
     # print(status.text)
-    #
+
     # # 打印关注名单
     # friends = api.GetFriends()
     # print([u.name for u in friends])
-	
+
     # # 打印Follower名单
     # followers = api.GetFollowers()
     # print([u.name for u in followers])
@@ -49,13 +53,34 @@ def main():
     # timeline = api.GetHomeTimeline(count=150)
     # print([tl.text for tl in timeline])
 
-    # # 删除Twitter
+    # # 删除200条推文，慎用！
     # statuses = api.GetUserTimeline(count=200)
     # for status in statuses:
     #     api.DestroyStatus(status._id)
 
+    # # 批量关注。名单来自name_list文件，每一行前为网名（昵称），后为注册用名，即screen_name，空格分隔
+    # with open('name_list.txt','r', encoding='gbk', errors='ignore') as f:
+    #     for line in f.readlines():
+    #         line = line.strip()
+    #         if not len(line):   # 跳过空行
+    #             continue
+    #         follow_id = line.split()[-1]
+    #         friend = api.CreateFriendship(screen_name=follow_id)
+    #         if friend:
+    #             print(friend.name)
 
-    # get up to 4000 latest tweets
+    # # 批量取消关注。使用名单同上，也可另建。
+    # with open('name_list.txt', 'r', encoding='gbk', errors='ignore') as f:
+    #     for line in f.readlines():
+    #         line = line.strip()
+    #         if not len(line):  # 跳过空行
+    #             continue
+    #         follow_id = line.split()[-1]
+    #         friend = api.DestroyFriendship(screen_name=follow_id)
+    #         if friend:
+    #             print(friend.name)
+
+    # 下载 4000 条推文（Twitter系统有限制，最多只能获取3200条）
     tweets = api.GetUserTimeline(screen_name=user, count=200)
     curr_id = tweets[-1].id
     for i in range(19):
